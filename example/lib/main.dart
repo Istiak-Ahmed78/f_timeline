9a3179bebe0b8f684e820b97e9da3cd9ed2dc51a
Istiak-Ahmed78 <aistiak2078@gmail.com>
2022-10-18 07:54:49 +0600

    Refactoring

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,34 +21,37 @@ class MyApp extends StatelessWidget {
 
 class FTimelineExample extends StatelessWidget {
   const FTimelineExample({Key? key}) : super(key: key);
-  final List<FTimeLineModel> timeLineModels = const [
-    FTimeLineModel(
-      title: 'Taken',
-      content: Center(
-        child: Text('Your order is taken'),
-      ),
-    ),
-    FTimeLineModel(
-      title: 'Processing',
-      content: Center(
-        child: Text('Your order is being processed'),
-      ),
-    ),
-    FTimeLineModel(
-      title: 'Packing',
-      content: Center(
-        child: Text('Hold on,We are making ready your product.'),
-      ),
-    ),
-    FTimeLineModel(
-      title: 'Way to',
-      content: Center(
-        child: Text('Just some moments. We are on the way'),
-      ),
-    ),
-  ];
+  List<FTimeLineModel> timeLineModels(BuildContext context) => [
+        FTimeLineModel(
+          title: 'Taken',
+          content: SizedBox(
+            height: MediaQuery.of(context).size.height * 1.3,
+            child: const Center(
+              child: Text('Your order is taken'),
+            ),
+          ),
+        ),
+        const FTimeLineModel(
+          title: 'Processing',
+          content: Center(
+            child: Text('Your order is being processed'),
+          ),
+        ),
+        const FTimeLineModel(
+          title: 'Packing',
+          content: Center(
+            child: Text('Hold on,We are making ready your product.'),
+          ),
+        ),
+        const FTimeLineModel(
+          title: 'Way to',
+          content: Center(
+            child: Text('Just some moments. We are on the way'),
+          ),
+        ),
+      ];
   @override
   Widget build(BuildContext context) {
-    return FTimeline(timelines: timeLineModels, doneTillIndex: 0);
+    return FTimeline(timelines: timeLineModels(context), doneTillIndex: 1);
   }
 }
